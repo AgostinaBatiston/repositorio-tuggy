d47472465293f27a75a3df5cd89abd119d2202a2
Agostina Batiston <agbatiston@hotmail.com>
2021-03-07 12:39:03 -0300

    Modifique el objeto de carrito

--- repositorio-tuggy.js
+++ repositorio-tuggy.js
@@ -1,28 +1,56 @@
 class ShoppingCart {
-    constructor(cart) {
+
+    constructor() {
+
         this.cart = [];
+
     }
 
-    getAdd = function() {
-        return this.cart.add;
-    }​
-    getRemove = function(id) {
-        return this.cart.remove;
-        // método para remover productos del carrito
-    }​
-    getCount = function() {
-        return this.cart.count;
-        // método para sacar la cantidad de elementos en el carrito
-    }​
 
-    getLength = function() {
-        return this.cart.length;
+
+    getAdd = function(prod) {
+
+        this.cart.push(prod);
+
     }
 
-    addValue = function(value) {
-        this.cart.push(value);
+
+
+    getFullCart = function() {
+
+        console.log('Tenés ' + this.cart.length + ' productos en el carrito');
+
+        for (var i = 0; i < this.cart.length; i++) {
+
+            console.log(this.cart[i]);
+
+        }
+
     }
+
 }
 
+
+
+class miProducto {
+
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+
+    }
+
+}
+
+var producto1 = new miProducto('Prod 1', $450);
+var producto2 = new miProducto('Prod 2', $800);
+
 var carrito = new ShoppingCart();
-carrito.addValue();
+
+carrito.getAdd(producto1);
+
+carrito.getAdd(producto2);
+
+
+
+carrito.getFullCart();
