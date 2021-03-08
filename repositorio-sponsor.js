3e5e49c980d84a2ed043880e0a15b01187566809
Agostina Batiston <agbatiston@hotmail.com>
2021-03-08 11:26:38 -0300

    Cree un repositorio para sponsor.html

---
+++ repositorio-sponsor.js
@@ -0,0 +1,7 @@
+var sponsor = prompt("Conoces/te gusta nerdomancers?");
+if (sponsor = "No") {
+    console.log("No es nerdovidente");
+} else {
+    alert("Bienvenido nerdovidente!");
+    console.log("Es un nerdovidente1 :D");
+}
